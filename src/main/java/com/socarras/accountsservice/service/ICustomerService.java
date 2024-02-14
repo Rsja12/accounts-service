dfc152780b4d201656d820f5d0e2f0068f9609e6
Alfonso <alfonso@mbp.local>
2024-02-14 12:21:32 -0500

    accept correlation id in api and pass through to downstream services

--- src/main/java/com/socarras/accountsservice/service/ICustomerService.java
+++ src/main/java/com/socarras/accountsservice/service/ICustomerService.java
@@ -4,5 +4,5 @@ import com.socarras.accountsservice.dto.CustomerDetailsDto;
 
 public interface ICustomerService {
 
-    CustomerDetailsDto fetchCustomerDetails(String mobileNumber);
+    CustomerDetailsDto fetchCustomerDetails(String mobileNumber, String correlationId);
 }
