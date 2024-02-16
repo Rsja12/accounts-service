663be123f1e45712cddb27ae0f8fdbb7438c55dc
Alfonso <alfonso@mbp.local>
2024-02-16 09:27:06 -0500

    add ratelimiter config

--- src/main/java/com/socarras/accountsservice/controller/AccountsController.java
+++ src/main/java/com/socarras/accountsservice/controller/AccountsController.java
@@ -6,6 +6,7 @@ import com.socarras.accountsservice.dto.CustomerDto;
 import com.socarras.accountsservice.dto.ErrorResponseDto;
 import com.socarras.accountsservice.dto.ResponseDto;
 import com.socarras.accountsservice.service.IAccountsService;
+import io.github.resilience4j.ratelimiter.annotation.RateLimiter;
 import io.github.resilience4j.retry.annotation.Retry;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.media.Content;
@@ -134,6 +135,7 @@ public class AccountsController {
             @ApiResponse(responseCode = "201", description = "Created"),
             @ApiResponse(responseCode = "500", content = @Content(schema = @Schema(implementation = ErrorResponseDto.class)))
     })
+    @RateLimiter(name = "getJavaVersion")
     @GetMapping("/java-version")
     public ResponseEntity<String> getJavaVersion() {
         return ResponseEntity
